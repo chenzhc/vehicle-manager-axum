ed799928786a6e66f5bb8902afe56f2cde2427b9
chenzhichao <80139283@qq.com>
2025-09-18 22:32:33 +0800

    add

--- src/main.rs
+++ src/main.rs
@@ -26,15 +26,46 @@ use tracing_subscriber::{
 async fn main() {
     vehicle_manager_axum::init();
 
+    // 1, Create the axum router
     let app = Router::new()
-        .route("/", get(|| async { "Hello, world!"}));
+        .route("/", get(|| async { "Hello, world!"}))
+        .route("/vehicle", 
+            get(vehicle_get)
+            .post(vehicle_post)
+        );
         // .layer(TraceLayer::new_for_http());
 
+    // 2, Define the IP and port listener (TCP)
     let addr = SocketAddr::from(([0, 0, 0, 0], 5000));
     info!("Starting server on {}", addr);
 
 
+    // 3, axum server to lunch the web server 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
     
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct Vehicle {
+    manufacturer: String,
+    model: String,
+    year: u32,
+    id: String,
+}
+
+async fn vehicle_get() -> Json<Vehicle> {
+    info!("Caller retrieved a vehicle from auxm");
+    Json::from(
+        Vehicle {
+            manufacturer: "Dodge".to_string(),
+            model: "RAM 1500".to_string(),
+            year: 2021,
+            id: uuid::Uuid::new_v4().to_string(),
+        }
+    )
+}
+
+async fn vehicle_post() {
+
 }
